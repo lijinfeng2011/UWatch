bb3774f461ee34c07aecd43bd9718d53ba6f134a
admin@local.host <admin@local.host>
2015-09-24 23:47:13 +0800

    watch_interval

--- src/watch_user.erl
+++ src/watch_user.erl
@@ -1,5 +1,7 @@
 -module(watch_user).
--export([start/0,add/2,del/1,list/0,auth/2,setindex/3,getindex/2,mesg/2]).
+-export([start/0,add/2,del/1,list/0,auth/2,setindex/3,getindex/2,mesg/2,getinterval/1]).
+
+-define(INTERVAL, 60).
 
 start() ->
   spawn( fun() -> refresh() end ).
@@ -28,6 +30,18 @@ auth(User,Passwd) ->
 setindex( User, Item, Index ) -> watch_db:set_userindex( User ++ "##" ++ Item, Index ).
 getindex( User, Item )        -> watch_db:get_userindex( User ++ "##" ++ Item ).
 
+getinterval( User ) ->
+  [Info] = watch_db:get_user_info(User),
+  InfoList = string:tokens(Info,"#"),
+  case catch lists:nth(4,InfoList ) of
+    {'EXIT',_} -> ?INTERVAL;
+    V -> 
+      case catch list_to_integer(V) of
+          {'EXIT',_} -> ?INTERVAL;
+          VV -> VV
+      end
+  end.
+
 mesg( User, Item ) ->
   Id = getindex( User,Item ),
   lists:map( 
@@ -48,25 +62,47 @@ mesg( User, Item ) ->
   watch_item:disk_log( Item, "mesg" )).
 
 refresh() ->
+  Time = watch_misc:milliseconds(),
   lists:map(
-      fun(X) -> {user,U,_} = X,
-         USER = list_to_atom( "user_list#"++ U ),
+      fun(X) ->
+         USER = list_to_atom( "user_list#"++ X ),
          case whereis( USER ) =:= undefined of
            true ->
-             Pid = spawn(fun() -> stored(U) end),
+             Pid = spawn(fun() -> stored(X) end),
              io:format("new ~p~n",[USER]),
              register( USER, Pid );
-           false -> USER ! { check }
+           false -> USER ! { check, Time }
          end
       end,
       watch_db:list_user()
   ),
-  timer:sleep( 60000 ),
+  timer:sleep( 5000 ),
   refresh().
 
 
-stored( NAME ) ->
+stored(NAME) ->
   receive
-    { check } -> io:format( "user ~p~n",[NAME])
+    { check, Time } -> 
+        UserMsec = watch_db:get_last("user#"++NAME),
+        UserInterval = watch_user:getinterval(NAME),
+     
+        case UserMsec + UserInterval > Time of
+            true ->
+                ItemList = watch_relate:list4user_itemnameonly(NAME),
+                AlarmList = lists:filter(
+                               fun(X) ->
+                                  TmpTime = watch_db:get_last("item#"++X),
+                                  (UserMsec< TmpTime) and ( TmpTime<Time)
+                               end, 
+                            ItemList ),
+
+               case length(AlarmList) > 0 of
+                   true -> watch_notify ! {NAME,AlarmList};
+                   false -> false      
+               end,
+               watch_db:set_last("user#"++NAME, Time);
+            false -> fase
+        end
+        
   end,
-  stored( NAME ).
+  stored(NAME).
