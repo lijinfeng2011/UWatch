6f48c8bc81fb51ca66879c50e9af7a246c1aa604
admin@local.host <admin@local.host>
2015-09-26 02:30:33 +0800

    ERL_MAX_ETS_TABLES 20000

--- bin/start.sh
+++ bin/start.sh
@@ -5,4 +5,4 @@
 path=$(dirname $0)
 cd "$path/../src/";
 
-erlc *.erl && erl -noshell -eval "watch:start(9999)."
+erlc *.erl && erl -noshell -eval "watch:start(9999)."  -sname abc -mnesia '"../data/db"'    -setcookie abc2323e2njnsdujsodisn -mnesia dump_log_write_threshold 50000 -mnesia dc_dump_limit 40  -env ERL_MAX_ETS_TABLES 20000
