511cdb3035091b5da2083dfa8d04dbbc37885a88
admin@local.host <admin@local.host>
2015-10-13 14:05:50 +0800

    notify add stat to web.alarm

--- src/watch_notify.erl
+++ src/watch_notify.erl
@@ -37,7 +37,8 @@ stored(Log) ->
 notify( User, AlarmList ) ->
     inets:start(),
     ssl:start(),
-    Info = string:join( AlarmList, "@" ),
+    AlarmList2 = lists:map( fun(X) -> X ++ ":"++ watch_db:get_stat(X) end, AlarmList),
+    Info = string:join( AlarmList2, "@" ),
     case httpc:request(post,{"http://127.0.0.1:7788/watch_alarm",
       [],"application/x-www-form-urlencoded", lists:concat(["user=" ,User ,"&info=",Info])},[],[]) of
     {ok, {_,_,Body}}-> Body, Stat = "ok";  
