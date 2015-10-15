a1a695a12ae74e708ff466995f6de8f47cd2e43d
lijinfeng <lijinfeng@360.cn>
2015-10-15 22:11:02 +0800

    mkdir ebin

--- bin/start.sh
+++ bin/start.sh
@@ -5,6 +5,7 @@ cd "$path/../";
 erl -make
 
 curP=$(pwd)
+mkdir "$curP/ebin/";
 cd "$curP/ebin/";
 
 erl -noshell -eval "watch:start(9999)."  -sname abc -mnesia '"../data/db"' -setcookie abc2323e2njnsdujsodisn -mnesia dump_log_write_threshold 50000 -mnesia dc_dump_limit 40  -env ERL_MAX_ETS_TABLES 20000
