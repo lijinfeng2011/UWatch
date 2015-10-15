0359c2afe86f787bc8e90d113b8b4d32fea1f73c
lijinfeng <lijinfeng@360.cn>
2015-10-15 11:18:48 +0800

    clean

--- bin/start.sh
+++ bin/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-
-
 path=$(dirname $0)
-cd "$path/../src/";
+cd "$path/../";
+erl -make
 
-erlc *.erl && erl -noshell -eval "watch:start(9999)."  -sname abc -mnesia '"../data/db"'    -setcookie abc2323e2njnsdujsodisn -mnesia dump_log_write_threshold 50000 -mnesia dc_dump_limit 40  -env ERL_MAX_ETS_TABLES 20000
+curP=$(pwd)
+cd "$curP/ebin/";
+
+erl -noshell -eval "watch:start(9999)."  -sname abc -mnesia '"../data/db"' -setcookie abc2323e2njnsdujsodisn -mnesia dump_log_write_threshold 50000 -mnesia dc_dump_limit 40  -env ERL_MAX_ETS_TABLES 20000
