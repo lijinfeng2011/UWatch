ea2380fe00ac3fb974945355d228ebb9aae5435e
lijinfeng2011 <lijinfeng2011@gmail.com>
2016-08-14 17:10:47 +0800

    添加参数把最大进程数改成10240000

--- server/bin/start.sh
+++ server/bin/start.sh
@@ -5,7 +5,6 @@ cd "$path/../";
 erl -make
 
 curP=$(pwd)
-mkdir -p "$curP/ebin/";
 cd "$curP/ebin/";
 
-erl -noshell -eval "watch:start(9999)."  -sname abc -mnesia '"../data/db"' -setcookie abc2323e2njnsdujsodisn -mnesia dump_log_write_threshold 50000 -mnesia dc_dump_limit 40  -env ERL_MAX_ETS_TABLES 20000
+erl -noshell -eval "watch:start(9999)."  -sname abc -mnesia '"../data/db"' -setcookie 123123resetcookie123123 -mnesia dump_log_write_threshold 50000 -mnesia dc_dump_limit 40  -env ERL_MAX_ETS_TABLES 20000  +P 10240000
