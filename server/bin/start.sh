2969ff2634919015a84905c3162b19ff285fe9ab
lijinfeng2011 <lijinfeng2011@gmail.com>
2016-08-16 18:27:09 +0800

    fix server/bin/start.sh

--- server/bin/start.sh
+++ server/bin/start.sh
@@ -6,6 +6,7 @@ erl -make
 
 curP=$(pwd)
 mkdir -p "$curP/ebin/";
+mkdir -p "$curP/data/"{db,item,notify};
 cd "$curP/ebin/";
 
 erl -noshell -eval "watch:start(9999)."  -sname abc -mnesia '"../data/db"' -setcookie 123123resetcookie123123 -mnesia dump_log_write_threshold 50000 -mnesia dc_dump_limit 40  -env ERL_MAX_ETS_TABLES 20000  +P 10240000
